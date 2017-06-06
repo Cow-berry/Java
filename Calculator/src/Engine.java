1b428cf2b588c36c9f10d4b7e05051bac05da07d
cowberry <tolya.cow@gmail.com>
2017-06-07 00:43:36 +0300

    remove unused import

--- Calculator/src/Engine.java
+++ Calculator/src/Engine.java
@@ -2,7 +2,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 
 public class Engine implements ActionListener {
 	
